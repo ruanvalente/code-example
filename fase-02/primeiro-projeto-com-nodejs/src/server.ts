cd364d1ddb26898a879123027c3183057c3af9c6
Ruan Valente <ruanopensuselinux@gmail.com>
2020-06-23 12:42:46 -0300

    feature created registration appointment :memo:

--- fase-02/primeiro-projeto-com-nodejs/src/server.ts
+++ fase-02/primeiro-projeto-com-nodejs/src/server.ts
@@ -1,12 +1,10 @@
 import express from 'express'
-import routes from './routes'
+
+import appointmentsRouter from './routes/appointments.routes'
 
 const app = express()
-
-app.use(routes)
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello World' })
-})
+app.use(express.json())
+app.use('/appointments', appointmentsRouter)
 
 app.listen(3333, () => {
   console.log('🚀 Server is running on http://localhost:3333')
