4ad2746d92eea4e1dd1f48a1c0747cca304aea49
Ruan Valente <ruanopensuselinux@gmail.com>
2020-06-23 13:06:11 -0300

    feature validating dates :memo:

--- fase-02/primeiro-projeto-com-nodejs/src/routes/appointments.routes.ts
+++ fase-02/primeiro-projeto-com-nodejs/src/routes/appointments.routes.ts
@@ -1,22 +1,36 @@
 import { Router } from 'express'
 import { uuid } from 'uuidv4'
+import { startOfHour, parseISO, isEqual } from 'date-fns'
 
 interface Appointments {
-  id?: string
+  id: string
   provider: string
-  date: string
+  date: Date
 }
 
 const appointmentsRouter = Router()
 
-const appointments = []
+const appointments: Appointments[] = []
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body
+
+  const parserDate = startOfHour(parseISO(date))
+
+  const findAppointmentsInSameDate = appointments.find(appointment =>
+    isEqual(parserDate, appointment.date),
+  )
+
+  if (findAppointmentsInSameDate) {
+    return response
+      .status(400)
+      .json({ message: 'This appointment is already booked' })
+  }
+
   const appointment = {
     id: uuid(),
     provider,
-    date,
+    date: parserDate,
   }
   appointments.push(appointment)
   return response.json(appointment)
